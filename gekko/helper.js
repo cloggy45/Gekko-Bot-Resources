48cd2ef2933e63d83f172cd78da567570d053f4a
root <cloggy45@googlemail.com>
2018-01-23 00:49:07 +0000

    updated the trailing stop loss, and changed the readme, still not finished

--- gekko/helper.js
+++ gekko/helper.js
@@ -1,76 +1,127 @@
 exports.trailingStopLoss = function () {
-    var _percent = 0.0;
-    var _buyPrice = 0;
-    var _stopLoss = 0;
-    var _active = false
 
-    var init = function (percent, buyPrice) {
-      _percent = percent;
-      _buyPrice = buyPrice;
-      _active = true;
-      _stopLoss = _buyPrice * _percent;
-    };
+    //WARNING THIS IS NOT FINISHED
 
-    var updateStopLoss = function (currentPrice) {
-        _stopLoss = currentPrice * _percent;
-    };
+    var _stopLoss = null;
+    var _buyPrice = null;
+    var _movingAverage = null;
+    var _active = false;
 
-    var triggered = function (currentPrice) {
-        return (_stopLoss > currentPrice)
+    var init_values = function (movingAverage, buyPrice) {
+        _movingAverage = movingAverage;
+        _buyPrice = buyPrice;
+        _active = true;
+        _stopLoss = movingAverage;
     };
 
-    var getStopLoss = function () {
-        return _stopLoss;
+    var update_our_stoploss = function (currentMovingAverage) {
+        _stopLoss = currentMovingAverage;
     };
-    
-    var resetValues = function () {
-      _percent = 0.0;
-      _buyPrice = 0;
-      _stopLoss = 0;
-      _active = false;
+
+    var calculate = function (currentPrice, currentMovingAverage) {
+        if(_active) {
+            if (currentPrice < currentMovingAverage )
+                return 'Current price is below';
+            else
+                update_our_stoploss(currentMovingAverage)
+        }
     };
-  
-    var isActive = function () {
-      return _active;
-    }
-  
+
+    var reset_settings = function () {
+        _stopLoss = null;
+        _buyPrice = null;
+        _movingAverage = null;
+        _active = false;
+    };
+
+    var log_values = function () {
+        console.log("Buy Price: " + _buyPrice);
+        console.log("Moving Average " + _movingAverage);
+        console.log("Stop Loss " + _stopLoss);
+    };
+
     return {
-        reset : resetValues,
-        get : getStopLoss,
-        create : init,
-        triggered : triggered,
-        update : updateStopLoss,
-        active : isActive
+        create: init_values,
+        reset: reset_settings,
+        update: update_our_stoploss,
+        triggered: calculate,
+        print: log_values
     }
 };
 
 exports.getStochCondition = function getCurrentCondition(stochk, stochd, lowThreshold, highThreshold) {
-  if (stochk > highThreshold && stochd > highThreshold)
-      return 'overbought';
+    if (stochk > highThreshold && stochd > highThreshold)
+        return 'overbought';
     else if (stochk < lowThreshold && stochd < lowThreshold)
-      return 'oversold';
+        return 'oversold';
     else
-      return 'middle';
-  };
+        return 'middle';
+};
 
 exports.getTrend = function getCurrentTrendDirection(ema, price) {
     if (ema > price)
-      return 'uptrend';
+        return 'uptrend';
     else if (ema < price)
-      return 'downtrend';
+        return 'downtrend';
     else
-      return 'no trend'
-  };
+        return 'no trend'
+};
 
-exports.crossover = function hasCrossedOver (shortEma, longEma) {
-  if(shortEma > longEma)
-    return 'short crossover'
-  else if(longEma > shortEma)
-    return 'long crossover'
-  else
-    return 'no crossover'
-}
+exports.crossover = function hasCrossedOver(shortEma, longEma) {
+    if (shortEma > longEma)
+        return 'short crossover'
+    else if (longEma > shortEma)
+        return 'long crossover'
+    else
+        return 'no crossover'
+};
 
- exports.trending = function isTrending(previousTrend, currentTrend) {
+/**
+ * We use this module to keep track of candle history.
+ */
+exports.candleHistory = function () {
+    var _limit = null;
+    var _candles = [];
+
+
+    var initialise = function (limit) {
+        _limit = limit - 1;
+    };
+
+    var _canAdd = function () {
+        return (_candles.length <= _limit);
+    };
+
+    var addCandleToarray = function (candle) {
+        if (_canAdd())
+            _candles.push(candle);
+        else {
+            _candles.shift();
+            _candles.push(candle);
+        }
+    };
+
+    var isFull = function () {
+        return (_candles.length >_limit);
+    };
+
+    var getCurrentCandles = function () {
+        return _candles;
+    };
+
+    var getCurrentSize = function () {
+        return _candles.length;
+    };
+
+    return {
+        init: initialise,
+        add: addCandleToarray,
+        get: getCurrentCandles,
+        full : isFull,
+        size : getCurrentSize
+    }
+};
+
+exports.trending = function isTrending(previousTrend, currentTrend) {
     return (previousTrend === currentTrend);
-  };
+};
